8f82e2a37f3453bed65374154e4b2bb23fd2031b
Scott Bowler <scott.bowler@learningfuze.com>
2019-06-30 01:14:44 +0000

    Sign in complete

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const mysql = require('mysql2/promise');
+const bcrypt = require('bcrypt');
+const { dbConfig } = require('./config');
 const PORT = process.env.PORT || 9000;
 
 const app = express();
@@ -6,69 +9,90 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.get('/', (request, response) => {
-    console.log('Request Received from:', request.url);
+const db = mysql.createPool(dbConfig);
 
-    response.send({
-        message: 'This is the home route',
+/* User Model
+{
+    name: '', 
+    email: '',
+    id: '',
+    password: '',
+    created_at: '',
+    updated_at: ''
+}
+*/
+
+app.post('/auth/sign-up', async (req, res) => {
+    const { email, name, password } = req.body;
+
+    const errors = [];
+
+    if(!email){
+        errors.push('No email provided');
+    }
+
+    if(!name){
+        errors.push('No name provided');
+    }
+
+    if(!password){
+        errors.push('No password provided');
+    } else if(password.length < 6){
+        errors.push('Password is too short');
+    }
+
+    if(errors.length){
+        return res.status(422).send({
+            errors: errors
+        });
+    }
+
+    const [[existingUser = null]] = await db.execute(
+        'SELECT id FROM users WHERE email=?',
+        [email]
+    );
+
+    if(existingUser){
+        return res.status(422).send({ error: 'Email already in use' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const [result] = await db.execute(
+        'INSERT INTO users (name, email, password, created_at, updated_at) VALUES (?, ?, ?, CURRENT_TIME, CURRENT_TIME)',
+        [name, email, hashedPassword]
+    );
+
+    res.send({
+        message: 'Account successfully created',
         user: {
-            name: 'George Hill',
-            email:  'george@email.com'
+            name,
+            userId: result.insertId
         }
     });
 });
 
-// GET /article
+app.post('/sign-in', async (req, res) => {
+    // check we received an email and password
 
-// const resp = {
-//     title: 'asd',
-//     content: '',
-//     author: {
-//         name: '',
-//         email: ''
-//     }
-// }
+    // no email and or password 422
 
-app.get('/extra-data', (req, res) => {
+    // Query DB for user with matching email
+    // No email found in DB 401
 
-    res.send({
-        message: 'Get query data',
-        queryData: req.query,
-        moreData: 'Here is more data'
-    });
-});
+    // use bcrypt to compare passwords
+    // Passwords don't match 401
 
-app.get('/article', (req, res) => {
-
-    res.send({
-        title: 'Some Article',
-        content: 'This is the content of the article',
-        author: {
-            name: 'James H',
-            email: 'james@example.com'
-        }
-    });
-});
-
-app.post('/sign-in', (req, res) => {
-    console.log('POST DATA:', req.body);
-
-    res.send({
-        message: 'You have signed in',
-        postData: req.body
-    });
-});
-
-// PATCH /update-user
-// PATCH DATA: name, email
-
-// OUTPUT { message: 'User updated', patchData: {}}
-
-app.patch('/update-user', (req, res) => {
-    res.send({
-        message: 'User updated',
-        patchData: req.body
-    });
+    // if everything matches send back
+    // {
+    //     message: 'Sign in success',
+    //         user: {
+    //         name: 'Sarah Conner',
+    //         userId: 9
+    //     }
+    // }
 });
 
 app.listen(PORT, () => {
