ad98422a6e14a52186c2a73effc633f74e3975a8
Scott Bowler <scott.bowler@learningfuze.com>
2019-07-24 02:19:27 +0000

    Completed updating with-cart to check for user cart, and created optional auth middleware

--- middleware/with_cart.js
+++ middleware/with_cart.js
@@ -7,41 +7,50 @@ module.exports = async (req, res, next) => {
         const { 'x-cart-token': cartToken } = req.headers;
         req.cart = null;
 
-        if(cartToken){
-            const cartData = jwt.decode(cartToken, cartSecret);
+        const [[cartStatus = null]] = await db.query(
+            'SELECT id FROM cartStatuses WHERE mid="active"'
+        );
 
-            const [cart = null] = await db.query(
-                `SELECT c.id AS cartId, c.lastInteraction, c.pid, c.createdAt,
+        if(!cartStatus){
+            throw new StatusError(500, 'Error retrieving cart data');
+        }
+
+        const cartQuery = `SELECT c.id AS cartId, c.lastInteraction, c.pid, c.createdAt,
                     c.updatedAt, c.userId, c.statusId AS cartStatusId,
                     ci.quantity, p.cost, p.id AS productId FROM carts AS c 
                     JOIN cartItems AS ci ON ci.cartId=c.id
                     JOIN products AS p ON ci.productId=p.id
-                    WHERE c.id=${cartData.cartId} AND c.deletedAt IS NULL AND ci.deletedAt IS NULL`
+                    WHERE c.deletedAt IS NULL AND ci.deletedAt IS NULL AND c.statusId=${cartStatus.id} `;
+        let cartWhere = null;
+
+        if(req.user){
+            cartWhere  = ` AND c.userId=${req.user.id}`;
+        } else if(cartToken){
+            const cartData = jwt.decode(cartToken, cartSecret);
+            cartWhere = ` AND c.id=${cartData.cartId}`;
+        }
+
+        if(cartWhere){
+            const [cart = null] = await db.query(
+                cartQuery + cartWhere
             );
 
-            if(!cart){
-                throw new StatusError(422, 'Invalid cart token');
+            if(cart){
+                const { cost, quantity, productId, ...cartItem } = cart[0];
+
+                const formattedCart = {
+                    ...cartItem,
+                    items: cart.map((item) => {
+                        return {
+                            cost: item.cost,
+                            id: item.productId,
+                            quantity: item.quantity
+                        }
+                    })
+                };
+
+                req.cart = formattedCart;
             }
-
-            const {cost, quantity, productId, ...cartItem} = cart[0];
-
-            // const formattedCart = {
-            //     ...cartItem,
-            //     items: cart.map(({ cost, quantity }) => ({ cost, quantity }))
-            // };
-
-            const formattedCart = {
-                ...cartItem,
-                items: cart.map((item) => {
-                    return {
-                        cost: item.cost,
-                        id: item.productId,
-                        quantity: item.quantity
-                    }
-                })
-            };
-
-            req.cart = formattedCart;
         }
         
         next();
